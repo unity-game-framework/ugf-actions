047350443ab15ebac1412bd69b934fa28fdcc717
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-01-04 17:03:52 +0300

    Change IActionContext

--- Packages/UGF.Actions/Runtime/IActionContext.cs
+++ Packages/UGF.Actions/Runtime/IActionContext.cs
@@ -7,12 +7,12 @@ namespace UGF.Actions.Runtime
     {
         void Add(object value);
         bool Remove(object value);
-        T Get<TArguments, T>(TArguments arguments, Func<TArguments, T, bool> predicate);
-        object Get<TArguments>(TArguments arguments, Func<TArguments, object, bool> predicate);
+        T Get<TArguments, T>(TArguments arguments, ActionContextPredicate<TArguments, T> predicate);
+        object Get<TArguments>(TArguments arguments, ActionContextPredicate<TArguments, object> predicate);
         T Get<T>() where T : class;
         object Get(Type type);
-        bool TryGet<TArguments, T>(TArguments arguments, Func<TArguments, T, bool> predicate, out T value);
-        bool TryGet<TArguments>(TArguments arguments, Func<TArguments, object, bool> predicate, out object value);
+        bool TryGet<TArguments, T>(TArguments arguments, ActionContextPredicate<TArguments, T> predicate, out T value);
+        bool TryGet<TArguments>(TArguments arguments, ActionContextPredicate<TArguments, object> predicate, out object value);
         bool TryGet<T>(out T value) where T : class;
         bool TryGet(Type type, out object value);
     }

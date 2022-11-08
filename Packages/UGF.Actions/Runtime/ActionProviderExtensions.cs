d39e1fd17465ef9fb7c8a219a8804e990934bece
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-08 15:43:27 +0100

    Add ActionProviderExtensions

---
+++ Packages/UGF.Actions/Runtime/ActionProviderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UGF.RuntimeTools.Runtime.Contexts;
+
+namespace UGF.Actions.Runtime
+{
+    public static class ActionProviderExtensions
+    {
+        public static void Execute<TCommand>(this IActionProvider provider, IContext context, ActionExecuteHandler<TCommand> handler) where TCommand : IActionCommand
+        {
+            if (provider == null) throw new ArgumentNullException(nameof(provider));
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (handler == null) throw new ArgumentNullException(nameof(handler));
+
+            if (provider.Current.TryGet(out IActionCommandList<TCommand> commands))
+            {
+                for (int i = 0; i < commands.Count; i++)
+                {
+                    TCommand command = commands.Get(i);
+
+                    handler.Invoke(provider, context, command);
+                }
+            }
+        }
+    }
+}
